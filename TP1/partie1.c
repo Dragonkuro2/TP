6c4d23093b67b210b9c2ede537d038c21a2add9b
Dragonkuro <Hicham.bourezi@gmail.com>
2024-10-18 16:06:48 +0100

    modifie la fonction ajouter_livre pour eveter les problemes de '\n' de stdin et ajout library string.h

--- TP1/partie1.c
+++ TP1/partie1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef struct {
+
+typedef struct livre {
 	int ID;
 	char title[256];
 	char author[120];
@@ -10,10 +13,11 @@ typedef struct {
 } livre;
 
 
-struct livre *ajoute_livre(struct livre *tail) {
-	struct *livre new_node;
+struct livre *ajouter_livre(struct livre *tail) {
+	struct livre *new_node = malloc(sizeof(livre));
 	// cette variable est pour verifie la disponiblite de livre
-	char dispo[2];
+	char dispo;
+
 	if (!new_node) {
 		printf("Error allocation de memoire!\n");
 		return (NULL);
@@ -21,13 +25,19 @@ struct livre *ajoute_livre(struct livre *tail) {
 
 	printf("Entrer l'id de livre: ");
 	scanf("%d", &new_node->ID);
+	getchar();
 	printf("Entrer le titre de livre: ");
-	fgets(new_node.title, sizeof(new_node.title), stdin);
+	fgets(new_node->title, sizeof(new_node->title), stdin);
+	// ce ligne est pour eveter le problem de '\n' de stdin
+	new_node->title[strcspn(new_node->title, "\n")] = '\0';
+
 	printf("Entrer l'auteur du livre: ");
-	fgets(new_node.author, sizeof(new_node.author), stdin);
+	fgets(new_node->author, sizeof(new_node->author), stdin);
+	new_node->author[strcspn(new_node->author, "\n")] = '\0';
+
 	printf("Est-ce que le livre est disponible (s'il disponible entrer 'y' sinon entrer une autre littre): ");
 	scanf("%c", &dispo);
-	new_node->disponible = (dispo == 'y') ? true : false;
+	new_node->disponible = (dispo == 'y');
 
 	if (!tail) {
 		new_node->next = new_node;
@@ -42,10 +52,10 @@ struct livre *ajoute_livre(struct livre *tail) {
 }
 
 int main() {
-	struct livre *tail;
+	struct livre *tail = malloc(sizeof(livre));
 	if (!tail) {
 		printf("Error allocation de memoire!\n");
-		return (NULL);
+		return (1);
 	}
 
 	ajouter_livre(tail);
