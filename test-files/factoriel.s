316ab122e60f505601896760a19d4b58fd6e9c49
Dragonkuro <Hicham.bourezi@gmail.com>
2024-10-19 13:31:54 +0100

    i changed the name of teh file

--- test-files/factoriel.s
+++ test-files/factoriel.s
@@ -1,4 +1,4 @@
-	.file	"exercice1.c"
+	.file	"factoriel.c"
 # GNU C17 (Ubuntu 13.2.0-23ubuntu4) version 13.2.0 (x86_64-linux-gnu)
 #	compiled by GNU C version 13.2.0, GMP version 6.3.0, MPFR version 4.2.1, MPC version 1.3.1, isl version isl-0.26-GMP
 
@@ -26,57 +26,57 @@ main:
 	movq	%rsp, %rbp	#,
 	.cfi_def_cfa_register 6
 	subq	$32, %rsp	#,
-# exercice1.c:3: int main() {
+# factoriel.c:3: int main() {
 	movq	%fs:40, %rax	# MEM[(<address-space-1> long unsigned int *)40B], tmp98
 	movq	%rax, -8(%rbp)	# tmp98, D.3189
 	xorl	%eax, %eax	# tmp98
-# exercice1.c:5: 	long long unsigned int result = 1;
+# factoriel.c:5: 	long long unsigned int result = 1;
 	movq	$1, -16(%rbp)	#, result
 .L2:
-# exercice1.c:8: 		printf("entrer une nombre pour calculer sont factoriel: ");
+# factoriel.c:8: 		printf("entrer une nombre pour calculer sont factoriel: ");
 	leaq	.LC0(%rip), %rax	#, tmp90
 	movq	%rax, %rdi	# tmp90,
 	movl	$0, %eax	#,
 	call	printf@PLT	#
-# exercice1.c:9: 		scanf("%d", &number);
+# factoriel.c:9: 		scanf("%d", &number);
 	leaq	-20(%rbp), %rax	#, tmp91
 	movq	%rax, %rsi	# tmp91,
 	leaq	.LC1(%rip), %rax	#, tmp92
 	movq	%rax, %rdi	# tmp92,
 	movl	$0, %eax	#,
 	call	__isoc99_scanf@PLT	#
-# exercice1.c:10: 	} while(number < 0);
+# factoriel.c:10: 	} while(number < 0);
 	movl	-20(%rbp), %eax	# number, number.0_1
 	testl	%eax, %eax	# number.0_1
 	js	.L2	#,
-# exercice1.c:12: 	while (number > 1) {
+# factoriel.c:12: 	while (number > 1) {
 	jmp	.L3	#
 .L4:
-# exercice1.c:13: 		result *= number;
+# factoriel.c:13: 		result *= number;
 	movl	-20(%rbp), %eax	# number, number.1_2
 	cltq
 	movq	-16(%rbp), %rdx	# result, tmp94
 	imulq	%rdx, %rax	# tmp94, tmp93
 	movq	%rax, -16(%rbp)	# tmp93, result
-# exercice1.c:14: 		number--;
+# factoriel.c:14: 		number--;
 	movl	-20(%rbp), %eax	# number, number.2_4
 	subl	$1, %eax	#, _5
 	movl	%eax, -20(%rbp)	# _5, number
 .L3:
-# exercice1.c:12: 	while (number > 1) {
+# factoriel.c:12: 	while (number > 1) {
 	movl	-20(%rbp), %eax	# number, number.3_6
 	cmpl	$1, %eax	#, number.3_6
 	jg	.L4	#,
-# exercice1.c:17: 	printf("le result est: %llu\n", result);
+# factoriel.c:17: 	printf("le result est: %llu\n", result);
 	movq	-16(%rbp), %rax	# result, tmp95
 	movq	%rax, %rsi	# tmp95,
 	leaq	.LC2(%rip), %rax	#, tmp96
 	movq	%rax, %rdi	# tmp96,
 	movl	$0, %eax	#,
 	call	printf@PLT	#
-# exercice1.c:18: 	return (0);
+# factoriel.c:18: 	return (0);
 	movl	$0, %eax	#, _15
-# exercice1.c:19: }
+# factoriel.c:19: }
 	movq	-8(%rbp), %rdx	# D.3189, tmp99
 	subq	%fs:40, %rdx	# MEM[(<address-space-1> long unsigned int *)40B], tmp99
 	je	.L6	#,
