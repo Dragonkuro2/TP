7cb9724634d6e165fe1e867f2e8ec27acad32018
Dragonkuro <Hicham.bourezi@gmail.com>
2024-10-29 12:10:08 +0100

    ajouter la fonction qui ajouter a la fin de la lsite

--- td-double-linked-list/exercice1.c
+++ td-double-linked-list/exercice1.c
@@ -17,7 +17,7 @@ struct node *ajouter_element_debut(struct node *head) {
 		return (head);
 	}
 
-	printf("Entrer un nombre a ajouter a la liste: ");
+	printf("Entrer un nombre a ajouter au debut de liste: ");
 	if (scanf("%d", &data) != 1) {
 		printf("Invalid input.\n");
 		free(new_node);
@@ -36,7 +36,44 @@ struct node *ajouter_element_debut(struct node *head) {
 		head = new_node;
 	}
 
-	printf("l'element %d a ete ajouter avec success a la listes.\n", data);
+	printf("l'element %d a ete ajouter avec success au debut de liste.\n", data);
+	return (head);
+}
+
+
+struct node *ajouter_element_fin(struct node *head) {
+	struct node *current = head;
+	struct node *new_node = malloc(sizeof(struct node));
+	int data;
+
+	if (!new_node) {
+		printf("Error allocation de memoire.\n");
+		return (head);
+	}
+
+	printf("Entrer une elemenet a ajouter au fin de liste: ");
+	if(scanf("%d", &data) != 1) {
+		printf("invalid input!.\n");
+		free(new_node);
+		return (head);
+	}
+
+	new_node->next = NULL;
+	new_node->data = data;
+
+	if (!head) {
+		new_node->prev = NULL;
+		head = new_node;
+	} else {
+		while (current->next) {
+			current = current->next;
+		}
+
+		new_node->prev = current;
+		current->next = new_node;
+	}
+
+	printf("l'elemenet %d a ete ajouter avec success a la fin de la liste.\n", data);
 	return (head);
 }
 
@@ -44,6 +81,7 @@ int main() {
 	struct node *head = NULL;
 
 	head = ajouter_element_debut(head);
+	head = ajouter_element_fin(head);
 
 	return (0);
 }
