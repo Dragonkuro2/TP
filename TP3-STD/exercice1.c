31410c2c8a5cac6983b3275b7ea1c034b0dea57a
Dragonkuro <Hicham.bourezi@gmail.com>
2024-12-06 18:05:54 +0100

    last version

--- TP3-STD/exercice1.c
+++ TP3-STD/exercice1.c
@@ -2,90 +2,87 @@
 #include <stdlib.h>
 #include <string.h>
 
-// structur of the conatct
+// Structure pour le contact
 typedef struct contact {
-	char name [100];
+	char name[100];
 	unsigned int num;
 } contact;
 
-// node of the contact
+// Noeud de l'arbre de contacts
 typedef struct Node {
 	contact contact;
 	struct Node *left;
 	struct Node *right;
 } Node;
 
-// function that gets the information of the contact from the user
+// Fonction pour obtenir les informations du contact de l'utilisateur
 contact *getContact() {
 	contact *new_contact = malloc(sizeof(contact));
 	if (!new_contact) {
-		printf("Error allocation de memoire.\n");
+		printf("Erreur d'allocation mémoire.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Entrer le nom de votre contact: ");
+	printf("Entrez le nom du contact : ");
 	scanf("%s", new_contact->name);
-	printf("Entrer le nemuro de votre contact: ");
+	printf("Entrez le numéro du contact : ");
 	scanf("%u", &new_contact->num);
 
-	return (new_contact);
+	return new_contact;
 }
 
-// function that create a new node of the contact tree
+// Fonction pour créer un nouveau noeud
 Node *create_node(contact *contact) {
 	Node *new_node = malloc(sizeof(Node));
-	
 	if (!new_node) {
-		printf("Error allocation de memoire.\n");
-		exit(1);
-	}	
+		printf("Erreur d'allocation mémoire.\n");
+		exit(EXIT_FAILURE);
+	}
 
 	new_node->contact = *contact;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	printf("new contact added.\n\tnom: %s\n\tnemuro: %u\n\n", contact->name, contact->num);
 
-	return (new_node);
+	printf("Nouveau contact ajouté :\n\tNom : %s\n\tNuméro : %u\n\n", contact->name, contact->num);
+	return new_node;
 }
 
-
-// function that insert the new node to the list
+// Fonction pour insérer un nouveau noeud dans l'arbre
 Node *insert(Node *tree, contact *contact) {
 	if (!tree) {
-		return (create_node(contact));
-	} else if ( strcmp(tree->contact.name , contact->name) > 0) {
+		return create_node(contact);
+	} else if (strcmp(tree->contact.name, contact->name) > 0) {
 		tree->left = insert(tree->left, contact);
 	} else {
 		tree->right = insert(tree->right, contact);
 	}
-	
-	return (tree);
+
+	return tree;
 }
 
-// recursive function that prints all the elements of the list
+// Fonction pour afficher tous les contacts
 void afficher(Node *tree) {
 	if (!tree)
 		return;
 
 	afficher(tree->left);
-	printf("\nNom: %s\nNemuro: %u\n", tree->contact.name, tree->contact.num);
+	printf("\nNom : %s\nNuméro : %u\n", tree->contact.name, tree->contact.num);
 	afficher(tree->right);
 }
 
-
-// this function shows an additional message that shows if the list is empty
+// Fonction d'affichage avec un message si l'arbre est vide
 void afficherAvecMessage(Node *tree) {
 	if (!tree) {
-		printf("La liste est vide!\n");
+		printf("La liste des contacts est vide !\n");
 		return;
 	}
 
 	afficher(tree);
 }
 
-// function that search for the entred name
+// Fonction pour rechercher un contact par nom
 Node *searchName(Node *tree, char *name) {
-	if (tree == NULL) {
+	if (!tree) {
 		printf("\nContact non trouvé.\n");
 		return NULL;
 	}
@@ -95,75 +92,84 @@ Node *searchName(Node *tree, char *name) {
 	else if (strcmp(tree->contact.name, name) < 0)
 		return searchName(tree->right, name);
 	else {
-		printf("\nContact trouvé:\n");
-		printf("Nom: %s\n", tree->contact.name);
-		printf("Phone: %u\n", tree->contact.num);
+		printf("\nContact trouvé :\n");
+		printf("Nom : %s\n", tree->contact.name);
+		printf("Numéro : %u\n", tree->contact.num);
+		return tree;
 	}
 }
 
-// function that askes the user to enter what he is looking for
+// Fonction pour demander à l'utilisateur de rechercher un contact
 void search(Node *tree) {
 	char name[30];
-	printf("Entrer le nom a chercher: ");
+	printf("Entrez le nom à rechercher : ");
 	scanf("%s", name);
 	searchName(tree, name);
 }
 
+// Fonction pour trouver le plus grand noeud dans le sous-arbre gauche
 Node *findLargestNode(Node *tree) {
-	if (tree == NULL || tree->right == NULL) {
+	if (!tree || !tree->right) {
 		return tree;
 	}
 
 	return findLargestNode(tree->right);
 }
 
-
+// Fonction pour supprimer un contact de l'arbre
 Node *delete(Node *tree, char *name) {
-	if (!tree)
-		printf("%s non trouve dans l'arbre\n");
-	else {
-		if (strcmp(name, tree->contact.name) < 0)
-			tree->left = delete(tree->right, name);
-		else if (strcmp(name, tree->contact.name) > 0) 
-			tree->right = delete(tree->right, name);
-		else {
-			if (tree->left && tree->right) {
-				Node *temp = findLargestNode(tree->left);
-				tree->contact.name = temp->contact.name;
-				tree->left = delete(tree->left, temp->contact.name);
+	if (!tree) {
+		printf("%s non trouvé dans l'arbre.\n", name);
+		return NULL;
+	}
+
+	if (strcmp(name, tree->contact.name) < 0) {
+		tree->left = delete(tree->left, name);
+	} else if (strcmp(name, tree->contact.name) > 0) {
+		tree->right = delete(tree->right, name);
+	} else {
+		if (tree->left && tree->right) {
+			Node *temp = findLargestNode(tree->left);
+			strcpy(tree->contact.name, temp->contact.name);
+			tree->contact.num = temp->contact.num;
+			tree->left = delete(tree->left, temp->contact.name);
+		} else {
+			Node *temp = tree;
+			if (tree->left) {
+				tree = tree->left;
 			} else {
-				Node *temp = tree;
-				if (tree->left)
-					tree = tree->left;
-				else
-					tree = tree->right;
+				tree = tree->right;
 			}
-			tree(temp);
+			free(temp);
 		}
 	}
+
+	return tree;
 }
 
-// main function
+// Fonction principale
 int main() {
 	Node *tree = NULL;
 	int choix;
 	char nameDelete[40];
 
 	do {
-		printf("\t************ Menu: ************\n");
-		printf("1- Inserer une nouvelle contact\n");
-		printf("2- rechercher une contact\n");
-		printf("3- afficher tout les contacts\n");
-		printf("4- supprimer un contact\n");
+		printf("\t************ Menu : ************\n");
+		printf("1- Insérer un nouveau contact\n");
+		printf("2- Rechercher un contact\n");
+		printf("3- Afficher tous les contacts\n");
+		printf("4- Supprimer un contact\n");
 		printf("5- Quitter\n");
-		printf("\n\tEntrer votre choix: ");
+		printf("\n\tEntrez votre choix : ");
 		scanf("%d", &choix);
 
 		switch (choix) {
-			case 1:
-				contact *Contact = getContact();
-				tree = insert(tree, Contact);
-				break;
+			case 1: {
+					contact *newContact = getContact();
+					tree = insert(tree, newContact);
+					free(newContact);
+					break;
+				}
 			case 2:
 				search(tree);
 				break;
@@ -171,17 +177,19 @@ int main() {
 				afficherAvecMessage(tree);
 				break;
 			case 4:
-				printf("entrer le nom a supprimer: ");
+				printf("Entrez le nom à supprimer : ");
 				scanf("%s", nameDelete);
-				delete(tree, nameDelete);
+				tree = delete(tree, nameDelete);
 				break;
 			case 5:
-				printf("See you next time!\n");
+				printf("À bientôt !\n");
 				break;
 			default:
-				printf("Entrer une valide option!\n\n");
+				printf("Entrez une option valide !\n\n");
 				break;
 		}
 	} while (choix != 5);
-	return (0);
+
+	return 0;
 }
+
