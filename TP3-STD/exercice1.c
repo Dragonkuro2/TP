f0fa16673290109c185308df6e284b337e89d069
Dragonkuro <Hicham.bourezi@gmail.com>
2024-12-06 15:47:34 +0100

    first version (it have some errors)

---
+++ TP3-STD/exercice1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct contact {
+	char name [100];
+	unsigned int num;
+} contact;
+
+typedef struct Node {
+	contact contact;
+	Node *left;
+	Node *right;
+} Node;
+
+contact *getContact() {
+	contact *new_contact = malloc(sizeof(contact));
+	if (!contact) {
+		printf("Error allocation de memoire.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Entrer le nom de votre contact: ");
+	scanf("%s", new_contact->name);
+	printf("Entrer le nemuro de votre contact: ");
+	scanf("%u", new_contact->num);
+
+	return (new_contact);
+}
+
+Node *create_node(contact *contact) {
+	Node *new_node = malloc(sizeof(Node));
+	
+	if (!new_node) {
+		printf("Error allocation de memoire.\n");
+		exit(1);
+	}	
+
+	new_node->contact = contact;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
+
+Node *insert(Node *tree, contact *contact) {
+	if (!tree) {
+		return (create_node(contact));
+	} else if (tree->name > contact->name) {
+		tree->left = insert(tree->left, contact);
+	} else {
+		tree->right = insert(tree->right, contact);
+	}
+	
+	printf("new contact added.\n\tnom: %s\n\tnemuro: %u\n\n", contact->name, contact->num);
+	return (tree);
+}
+
+int main() {
+	Node *tree = NULL;
+
+	contact *Contact = getContact();
+	tree = insert(tree, contact);
+
+	return (0);
+}
